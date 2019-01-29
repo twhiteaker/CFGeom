7087dd785367021f6e605cfe2087b29e2ee421a4
Tim Whiteaker <whiteaker@utexas.edu>
2019-01-29 10:08:35 -0600

    switch from distutils to setuptools for install

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='CFGeom',
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cfgeom',
     version='0.2.0',
     author='TIm Whiteaker',
     author_email='whiteaker@utexas.edu',
-    packages=['cfgeom', 'cfgeom.test'],
+    packages=setuptools.find_packages(),
     scripts=['bin/json_to_examples.py'],
-    url='http://pypi.python.org/pypi/CFGeom/',
+    url='https://github.com/twhiteaker/CFGeom',
     license='LICENSE',
     description='Reference implementation for representing geometries in NetCDF with the CF Conventions',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=[
         'shapely >= 1.6.4.post1',
+        'numpy >= 1.9.3',
         'netcdf4 >= 1.0.8',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
