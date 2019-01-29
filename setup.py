6824e505479cc0ccf1fc1815cc8683b802ccbd0f
Tim Whiteaker <whiteaker@utexas.edu>
2019-01-29 11:58:45 -0600

    Add version attribute

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
+import re
 import setuptools
+
+
+def get_version():
+    VERSIONFILE = 'cfgeom/_version.py'
+    with open(VERSIONFILE, 'rt') as fh:
+        verstrline = fh.read()
+    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+    mo = re.search(VSRE, verstrline, re.M)
+    if mo:
+        verstr = mo.group(1)
+    else:
+        m = 'Unable to find version string in %s.' % (VERSIONFILE,)
+        raise RuntimeError(m)
+    return verstr
+
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='cfgeom',
-    version='0.2.0',
+    version=get_version(),
     author='TIm Whiteaker',
     author_email='whiteaker@utexas.edu',
     packages=setuptools.find_packages(),
-    scripts=['bin/json_to_examples.py'],
+    scripts=[],
     url='https://github.com/twhiteaker/CFGeom',
     license='LICENSE',
     description='Reference implementation for representing geometries in NetCDF with the CF Conventions',
